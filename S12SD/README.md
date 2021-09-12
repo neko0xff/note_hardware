766505597af42d8e7d33e42327967d6f73ff7138
zangmen <zangmenhsu@yahoo.co.jp>
2021-09-12 19:30:34 +0800

    20210912 update 2

--- S12SD/README.md
+++ S12SD/README.md
@@ -1,1 +1,27 @@
 # S12SD
+## 功能
+* 類型:太陽紫外線強度感測器
+## 特性
+* 適合於檢測在太陽光的UV輻射。它可以用於任何需要監視UV光量的應用，並且可以很容易地連接到任何微控制器
+## 規格
+* 尺寸小：11mm×27mm
+* 功耗低：供電電壓2.5V～5V，工作電流是微安級
+* 線性好
+* 靈敏度高
+* 高穩定性
+* 檢測範圍寬：240nm-370nm
+* 大角度：130度
+* 肖特基種類的光敏二極管，適用於光電模式
+## 運行方式
+* 連接到ADC輸入並讀取該值
+## 電壓転換公式
+* VCC=5V
+```
+sensorVoltage = sensorValue / 1024 * 3.3
+```
+* VCC=3.3V
+```
+sensorVoltage = sensorValue / 1024 * 5
+```
+## source
+* https://www.taiwaniot.com.tw/product/guva-s12sd-%E5%A4%AA%E9%99%BD%E7%B4%AB%E5%A4%96%E7%B7%9A%E5%BC%B7%E5%BA%A6%E6%84%9F%E6%B8%AC%E5%99%A8-%E5%81%B5%E6%B8%AC%E7%B4%AB%E5%A4%96%E7%B7%9A%E5%BC%B7%E5%BA%A6/
--- S12SD/sketch_sep10a/sketch_sep10a.ino
+++ S12SD/sketch_sep10a/sketch_sep10a.ino
@@ -8,12 +8,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float sensorVoltage,sensorValue;
+  float sensorVoltage;
+  int sensorValue;
   sensorValue = analogRead(A0);
   /*
    * 転換公式: sensorVoltage = sensorValue/1024* 供電電壓 (3.3V 或 5V) 
   */
-  sensorVoltage = sensorValue/1024*3.3;
+  sensorVoltage = (float)sensorValue/1024*3.3;
 
   
   Serial.print("Sensor Reading=");
@@ -23,25 +24,26 @@ void loop() {
   Serial.print(sensorVoltage);
   Serial.println(" V");
   Serial.print("UV=");
-  Serial.println(mv_to_uv(sensorVoltage));
+  Serial.println(mv_to_uv(sensorValue));
   
-  delay(1000);
+  //delay(2000);
 }
 
-int mv_to_uv(float sensor_value){
+int mv_to_uv(int sensor_value){
    int uv;
    
-   if(sensor_value<=1170) uv=11;
-   else if(sensor_value<=1079) uv=10;
-   else if(sensor_value<=976) uv=9;
-   else if(sensor_value<=881) uv=8;
-   else if(sensor_value<=795) uv=7;
-   else if(sensor_value<=696) uv=6;
-   else if(sensor_value<=606) uv=5;
-   else if(sensor_value<=503) uv=4;
-   else if(sensor_value<=408) uv=3;
-   else if(sensor_value<=318) uv=2;
-   else if(sensor_value<=227) uv=1;
-   else if(sensor_value=50)  uv=0;
+   if(sensor_value<=226) uv=0;
+   else if(sensor_value<=227&&sensor_value<=317) uv=1;
+   else if(sensor_value<=318&&sensor_value<=407) uv=2;
+   else if(sensor_value<=408&&sensor_value<=502) uv=3;
+   else if(sensor_value<=503&&sensor_value<=605) uv=4;
+   else if(sensor_value<=606&&sensor_value<=695) uv=5;
+   else if(sensor_value<=696&&sensor_value<=794) uv=6;
+   else if(sensor_value<=795&&sensor_value<=880) uv=7;
+   else if(sensor_value<=881&&sensor_value<=975) uv=8;
+   else if(sensor_value<=976&&sensor_value<=1078) uv=9;
+   else if(sensor_value<=1079&&sensor_value<=1169) uv=10;
+   else uv=11;
    
+   return uv;
 }
